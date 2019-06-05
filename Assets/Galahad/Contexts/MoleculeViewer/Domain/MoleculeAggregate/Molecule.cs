9cf919cd34f904fb1d4f84862f1f133fa3bb9cf3
Yusuke Kawashima <kawashimayusuke113@gmail.com>
2019-06-05 14:30:01 +0900

    make MoleculeViewer context

---
+++ Assets/Galahad/Contexts/MoleculeViewer/Domain/MoleculeAggregate/Molecule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Galahad.Contexts.MoleculeViewer.Domain.ValueObject;
+using UnityEngine;
+
+namespace Galahad.MoleculeViewerContext.Domain.MoleculeAggregate
+{
+    [Serializable]
+    public class Molecule : ISerializationCallbackReceiver
+    {
+        [SerializeField] private List<Atom> atoms;
+        [SerializeField] private List<Bond> bonds;
+        [SerializeField] private string identifier;
+        [SerializeField] private Vector3 offsetPosition;
+        [SerializeField] private string title;
+
+        public Molecule(Atoms atoms, Bonds bonds, Position offsetPosition, string title)
+        {
+            Atoms = atoms;
+            Bonds = bonds;
+            Identifier = Guid.NewGuid().ToString();
+            OffsetPosition = offsetPosition;
+            Title = title;
+        }
+
+        public Position OffsetPosition { get; private set; }
+
+        public string Identifier { get; private set; }
+        public string Title { get; private set; }
+        public Atoms Atoms { get; private set; }
+        public Bonds Bonds { get; private set; }
+
+        public void OnBeforeSerialize()
+        {
+            identifier = Identifier;
+            title = Title;
+            atoms = Atoms?.ToList();
+            bonds = Bonds?.ToList();
+            offsetPosition = OffsetPosition?.Value ?? Vector3.zero;
+        }
+
+        public void OnAfterDeserialize()
+        {
+            Identifier = identifier ?? Guid.NewGuid().ToString();
+            Title = title;
+            Atoms = new Atoms(atoms);
+            Bonds = new Bonds(bonds);
+            OffsetPosition = new Position(offsetPosition);
+        }
+    }
+}
---
+++ Assets/Galahad/Scripts/Editor/MoleculeRepositoryInspector.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using Galahad.MoleculeViewerContext.Domain.MoleculeAggregate;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Galahad.Scripts.Editor
+{
+    [CustomEditor(typeof(MoleculeRepository))]
+    public class MoleculeRepositoryInspector : UnityEditor.Editor
+    {
+        private static MoleculeRepository _moleculeRepository;
+        private static IEnumerator _iEnumerator;
+        private string _json;
+        private string _url = "http://0.0.0.0:8080";
+
+        private void OnEnable()
+        {
+            _moleculeRepository = target as MoleculeRepository;
+            EditorApplication.update += Update;
+        }
+
+        private static void Update()
+        {
+            if (_iEnumerator == null) return;
+            while (_iEnumerator.MoveNext())
+            {
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            _json = EditorGUILayout.TextField(_json);
+            if (GUILayout.Button("Add Json Molecule"))
+                _moleculeRepository.AddMolecule(JsonUtility.FromJson<Molecule>(_json));
+
+            _url = EditorGUILayout.TextField(_url);
+            if (GUILayout.Button("Add Molecule From HTML")) _iEnumerator = GetMoleculeJsonFromHttp(_url);
+        }
+
+        public IEnumerator GetMoleculeJsonFromHttp(string uri)
+        {
+            var request = UnityWebRequest.Get(uri);
+            yield return request.SendWebRequest();
+
+            while (!request.isDone) yield return 0;
+            Debug.Log(request.responseCode);
+            Debug.Log(request.downloadHandler.text);
+
+            _moleculeRepository.AddMolecule(JsonUtility.FromJson<Molecule>(request.downloadHandler.text));
+            _iEnumerator = null;
+        }
+    }
+}
