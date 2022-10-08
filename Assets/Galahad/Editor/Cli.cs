d4a95d5e48855eb934e42f143b84fa7030743df4
Yusuke Kawashima <kawashimayusuke113@gmail.com>
2022-10-08 16:46:45 +0900

    [fix] remove unexpected codes

--- Assets/Galahad/Editor/Cli.cs
+++ Assets/Galahad/Editor/Cli.cs
@@ -36,8 +36,23 @@ namespace Galahad.Editor
             {
                 throw new Exception();
             }
-            Debug.Log(args[0]);
-            Debug.Log(args[1]);
+            var moleculeRepository = AssetDatabase.LoadAssetAtPath<MoleculeRepository>(args[0]);
+            var moleculeRepositoryCopy = GameObject.Instantiate<MoleculeRepository>(moleculeRepository);
+            moleculeRepositoryCopy.AddMoleculeFromSmiles(args[1]);
+            var tempFile =  Path.GetFileNameWithoutExtension(args[0]) + "_org";
+            AssetDatabase.RenameAsset(args[0], tempFile);
+            AssetDatabase.CreateAsset(moleculeRepositoryCopy, args[0]);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.DeleteAsset(Path.Combine(Path.GetDirectoryName(args[0]), tempFile + ".asset"));
+        }
+
+        public static void AddSdfIntoMoleculeRepository()
+        {
+            var args = TrimArgs(System.Environment.GetCommandLineArgs());
+            if (args.Length < 2)
+            {
+                throw new Exception();
+            }
             var moleculeRepository = AssetDatabase.LoadAssetAtPath<MoleculeRepository>(args[0]);
             var moleculeRepositoryCopy = GameObject.Instantiate<MoleculeRepository>(moleculeRepository);
             moleculeRepositoryCopy.AddMoleculeFromSmiles(args[1]);
